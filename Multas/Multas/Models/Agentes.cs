9204838a794ab07c00e8ca8d07d8c51f0ad6c18e
Joao Almeida <aluno20467@ipt.pt>
2019-04-02 17:00:52 +0100

    Criacao inicial

---
+++ Multas/Multas/Models/Agentes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Multas.Models
+{
+    public class Agentes
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+        public string Esquadra { get; set; }
+        public string Foto { get; set; }
+
+
+    }
+}
---
+++ Multas/Multas/Models/MultasDB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Multas.Models
+{ 
+
+    public class MultasDB: DbContext {
+        //identificar qual o SGBD a usar 
+
+
+
+
+        //definir as tabelas da BD 
+
+        public DbSet<Condutores> Condutores { get; set; }
+        public DbSet<Viaturas> Viaturas { get; set; }
+        public DbSet<Multas> Multas { get; set; }
+        public DbSet<Agentes> Agentes { get; set; }
+
+
+
+
+    }
+
+}
