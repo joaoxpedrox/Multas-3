e7923f95c1ebb93eed22e9caa1727c32ad5bde7b
Joao Almeida <aluno20467@ipt.pt>
2019-06-04 15:01:14 +0100

    Conclusºao da junção das bases de dados de autenticação e do negócio numa unica base de dados

--- Multas/Multas/Models/Agentes.cs
+++ Multas/Multas/Models/Agentes.cs
@@ -8,54 +8,38 @@ namespace Multas.Models
 {
     public class Agentes
     {
+
         public Agentes()
         {
-            ListaDeMultas = new HashSet<Multas>(); 
+            ListaDeMultas = new HashSet<Multas>();
         }
-        [Key] //identifica este atributo como Primary Key 
+
+
+        [Key] // identifica este atributo como Primary Key
         public int ID { get; set; }
 
-       // [Required]
-        //Se quiser que a mensagem de erro venha em X lingua, devo     
-        //[Required (ErrorMessage="O nome é de preenchimento obrigatório")] 
-       // [StringLength(50, ErrorMessage ="O {0} deve ter, no máximo, {1} caracteres")]
-      //  [RegularExpression(""  , ErrorMessage ="O {0} só pode conter letras. Cada palavra deve começar com uma Maiúscula")]
-
-        //[]- Representa um conjunto, de onde sai apenas um símbolo 
-        // + - Representa uma, ou mais ocorrªencias
-        // * representa zero, ou mais, ocorrências 
-        // ? representa zero ou uma ocorrência 
-        //  [A-Z][a-z]+  (( | e | de | do | dos | da | das ) [A-Z][a-z]+)*
-        //          ( ((e|de|do|dos|da|das) )?)
-        //          ( (((e|d(a|o)s?|e)) )?)
-
-
-
-
-        [Required(ErrorMessage = "o Nome é de preenchimento obrigatório.")]
-        [StringLength(40)]
-        [RegularExpression("[A-ZÁÉÍÓÚÂ][a-záéíóúàèìòùäëïöüãõâêîôûçñ]+(( | e | de | da | das | do | dos |-|'|d')[A-ZÁÉÍÓÚÂ][a-záéíóúàèìòùäëïöüãõâêîôûçñ]*){1,3}",
-                         ErrorMessage = "só são aceites palavras, começadas por Maiúscula, " +
-                                       "separadas por um espeço em branco.")]
+        [Required(ErrorMessage = "O Nome é de preenchimento obrigatório.")]
+        [StringLength(50, ErrorMessage = "O {0} deve ter, no máximo, {1} caracteres.")]
+        //[RegularExpression("[A-ZÁÉÍÓÚ][a-záéíóúàèìòùäëïöüãõâêîôûçñ]+(( | e | de | do | dos | da | das |-|')[A-ZÁÉÍÓÚ][a-záéíóúàèìòùäëïöüãõâêîôûçñ]+)*",
+        //                    ErrorMessage = "O {0} só pode conter letras. Cada palavra deve começar com uma Maiúscula.")]
         public string Nome { get; set; }
 
-        [Required(ErrorMessage = "a Esquadra é de preenchimento obrigatório.")]
-        [StringLength(30)]
-        [RegularExpression("Torres Novas|Tomar|Entroncamento",
-                           ErrorMessage = " só é aceite Torres Novas ou Tomar ou Entroncamento")]
+        [Required(ErrorMessage = "A Esquadra é de preenchimento obrigatório.")]
+        [StringLength(20, ErrorMessage = "A {0} deve ter, no máximo, {1} caracteres.")]
+        //[RegularExpression("[A-Z][a-z]+(( |-)[A-Z][a-z]+)*",
+        //                    ErrorMessage = "O {0} só pode conter letras. Cada palavra deve começar com uma Maiúscula.")]
         public string Esquadra { get; set; }
 
-        [StringLength(30)]
+
         public string Fotografia { get; set; }
 
+        // *************************************
 
         /// <summary>
-        /// lista das multas associadas ao Agente
+        ///  lista das multas associadas ao Agente
         /// </summary>
         public virtual ICollection<Multas> ListaDeMultas { get; set; }
-        //este termo 'virtual' ativar a funcionalidade de 'lazy loading' 
-
-
+        // este termo 'virtual' vai ativar a funcionalidade de 'lazy loading'
 
     }
 }
---
+++ Multas/Multas/Models/IdentityModels.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Multas.Models
+{
+
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+
+    }
+
+
+    /// <summary>
+    /// apresenta a criação da base de dados
+    /// - dos utilizadores
+    /// - dos dados do 'negócio'
+    /// </summary>
+    public class MultasDB : IdentityDbContext<ApplicationUser>
+    {
+
+        // construtor que identifica a localização da BD  
+        public MultasDB()
+               : base("MultasDbConnectionString", throwIfV1Schema: false)
+        {
+        }
+
+        static MultasDB()
+        {
+            // Set the database intializer which is run once during application start
+            // This seeds the database with admin user credentials and admin role
+            Database.SetInitializer<MultasDB>(new ApplicationDbInitializer());
+        }
+
+        public static MultasDB Create()
+        {
+            return new MultasDB();
+        }
+
+        // adicionar aqui as tabelas da BD
+        // definir as tabelas da BD
+        public DbSet<Condutores> Condutores { get; set; }
+        public DbSet<Viaturas> Viaturas { get; set; }
+        public DbSet<Agentes> Agentes { get; set; }
+        public DbSet<Multas> Multas { get; set; }
+
+
+        // método a ser executado no início da criação do Modelo
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // eliminar a convenção de atribuir automaticamente o 'on Delete Cascade' nas FKs
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+}
